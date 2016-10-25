d352644efffb156a33002c9ac005653d105e3c69
apm5127 <Adam.McHenry@meppi.com>
2016-10-25 14:08:08 -0400

    Partial implementation. Added AppRouting module.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,22 +1,34 @@
+// Import Angular2 modules here
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
+// Import components here
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component'
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+
+// Import Services here
+import { HeroService } from './hero.service';
 
 @NgModule({
 	imports: [
 		BrowserModule,
-		FormsModule
+		FormsModule,
 	],
 	declarations: [
 		AppComponent,
+		DashboardComponent,
+		HeroesComponent,
 		HeroDetailComponent
+	],
+	providers: [
+		HeroService
 	],
 	bootstrap: [
 		AppComponent
 	]
 })
-
 export class AppModule { }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,11 +1,20 @@
+// Import Angular2 modules here
 import { Injectable } from '@angular/core';
 
+// Import components here
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+
+// Import Services here
+// import {  } from 'module';
 
 @Injectable()
 export class HeroService {
 	getHeroes(): Promise<Hero[]> {
 		return Promise.resolve(HEROES);
+	}
+
+	getHero(id: number): Promise<Hero> {
+		return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
 	}
 }
